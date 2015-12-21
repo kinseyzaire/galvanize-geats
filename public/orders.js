b7ee7cb7529c08cf1c98acdd2f4ae94c9eb22fba
Kinsey Ashby <kinseyzaire@gmail.com>
2015-12-21 11:55:18 -0700

    started order form

---
+++ public/orders.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+   var menuOnThePage = $("#eatThisMenu")
+   var menu = $.ajax( {
+      url:"https://galvanize-eats-api.herokuapp.com/menu",
+      method:"GET",
+      dataType: "json",
+      success: function(data) {
+      console.log(data);
+      console.log(data.menu[0]["name"]);
+      console.log(data.menu.length);
+      for (var i = 0; i < data.menu.length; i++) {
+         menuOnThePage.append("<option id='menuItem"+[i]+"'>"+data.menu[i]['name']+"</option>")
+      }
+
+      }
+   });
+});
