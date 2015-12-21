e0ef100989359e899dbb106bf77f1a00e2139162
Kinsey Ashby <kinseyzaire@gmail.com>
2015-12-21 15:41:11 -0700

    order form progress

--- public/app/pages/orders.js
+++ public/app/pages/orders.js
@@ -1,4 +1,5 @@
 var thePull = [];
+var thePush = [];
 
 $(document).ready(function(){
    var menuOnThePage = $("#eatThisMenu")
@@ -15,11 +16,27 @@ $(document).ready(function(){
    });
 });
 
+var subtotal = 0;
+var orderTaxxx = 0;
 $("#addToOrder").click(function(){
    var itemToAdd = $("#eatThisMenu").val()
    var amount = $("#qty").val()
    var orderTable = $("#putAllTheOrderHere")
    for (var i = 0; i < amount; i++) {
       orderTable.append("<tr><td>"+thePull[itemToAdd]['name']+"</td><td>"+thePull[itemToAdd]['price']+"</td></tr>");
+      thePush.push(thePull[itemToAdd]);
+      console.log(subtotal);
+      console.log(thePull[itemToAdd]['price']);
+      subtotal += (thePull[itemToAdd]['price'])*1;
+      orderTaxxx += ((thePull[itemToAdd]['price'])*.07)*1;
    }
+   var grandddTotal = (subtotal*1) + (orderTaxxx*1);
+   $("#orderSubtotal").html(subtotal);
+   $("#orderTax").html(orderTaxxx.toFixed(2));
+   $("#orderGrandTotal").html(grandddTotal.toFixed(2));
+   console.log(thePush);
+   console.log(subtotal);
+   console.log(orderTaxxx);
+   console.log(grandddTotal);
+   $("#theDeets").animate({"opacity":1.0},500,function(){console.log()});
 });
